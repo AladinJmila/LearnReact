4ead0bd6cd4470144b4a6d1558d9d777a672d0de
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-04 18:09:10 +0000

    Anatomy Of a Component

---
+++ BradTraversy/anatomyOfaComponent.js
@@ -0,0 +1,50 @@
+
+Class Post extends React.Component {
+  state = {
+    title: 'Post One',
+    body: 'This is my post'
+  }
+
+  render() {
+    return(
+      <div>
+        <h3>{ this.state.title }</h3>
+        <p>{ this.state.body }</p>
+      </div>
+    )
+  }
+}
+
+/**Read text**/
+
+
+
+/**Read text**/
+
+
+
+/**Read text**/
+
+
+
+/**Read text**/
+
+
+
+/**Read text**/
+
+
+
+/**Read text**/
+
+
+
+/**Read text**/
+
+
+
+/**Read text**/
+
+
+
+/**Read text**/

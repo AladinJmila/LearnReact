7157474c135f580b9b1637f2c5d50eef5729e78e
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-05 20:32:01 +0000

    Finished Typing for today

---
+++ BradTraversy/gettingStarted.js
@@ -0,0 +1,17 @@
+export const Header = () => {
+  return(
+    <div>
+      <h1>My Header</h1>
+    </div>
+  )
+}
+
+export default class Header extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>My Header</h1>
+      </div>
+    )
+  }
+}

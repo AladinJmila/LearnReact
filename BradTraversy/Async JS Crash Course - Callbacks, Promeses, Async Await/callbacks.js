76aa8ef4cae520e4c0177392d69599bc9c293de3
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-06 15:54:35 +0000

    Async JS Crash Course - Callbacks, Promises, Async Await

---
+++ BradTraversy/Async JS Crash Course - Callbacks, Promeses, Async Await/callbacks.js
@@ -0,0 +1,43 @@
+
+const posts = [
+  { title: 'Post One', body: 'This is post one' },
+  { title: 'Post Tow', body: 'This is post two' },
+]
+
+/**Read text**/
+
+function getPosts() {
+  setTimeout(fuction() {
+
+  }, 1000)
+}
+
+/**Read text**/
+
+
+
+/**Read text**/
+
+
+
+/**Read text**/
+
+
+
+/**Read text**/
+
+
+
+/**Read text**/
+
+
+
+/**Read text**/
+
+
+
+/**Read text**/
+
+
+
+/**Read text**/

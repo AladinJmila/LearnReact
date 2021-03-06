23ad573cf2285a5bb746a1cc819ac3a235ff54ce
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-06 21:22:13 +0000

    After more typing...1

--- BradTraversy/Async JS Crash Course - Callbacks, Promeses, Async Await/callbacks.js
+++ BradTraversy/Async JS Crash Course - Callbacks, Promeses, Async Await/callbacks.js
@@ -1,40 +1,73 @@
 
 const posts = [
-  { title: 'Post One', body: 'This is post one' },
-  { title: 'Post Tow', body: 'This is post two' },
+  { title: 'Post 1', body: 'This is post one' },
+  { title: 'Post 2', body: 'This is post two' },
+  { title: 'Post 3', body: 'This is post three' },
+  { title: 'Post 4', body: 'This is post four' },
 ]
 
 /**Read text**/
 
-function getPosts() {
-  setTimeout(fuction() {
+// function getPosts() {
+//   setTimeout(fuction() {
+//
+//   }, 1000)
+// }
 
-  }, 1000)
+/**Read text**/
+
+// function getPosts() {
+//   setTimeout(() => {
+//
+//   },1000)
+// }
+
+/**Read text**/
+
+// function getPosts() {
+//   setTimeout(() => {
+//     let output = ''
+//   },1000)
+// }
+
+/**Read text**/
+
+// function getPosts() {
+//   setTimeout(() => {
+//     let output = ''
+//       posts.forEach((post, index) => {
+//         output += `<li>${post.title}</li>`
+//       })
+//   },1000)
+// }
+
+/**Read text**/
+
+function getPosts() {
+  setTimeout(() => {
+    let output = ''
+      posts.forEach((post, index) => {
+        output += `<li>${post.title}</li>`
+      })
+      document.body.innerHTML = output
+  },1000)
 }
 
 /**Read text**/
 
-
-
-/**Read text**/
-
-
+getPosts()
 
 /**Read text**/
 
+function createPost(post) {
+  setTimeout(() => {
+    posts.push(post)
+    console.log(posts)
+  }, 2000)
+}
 
-
-/**Read text**/
-
-
-
-/**Read text**/
-
-
-
-/**Read text**/
-
-
+const newPost =  { title: 'Post 5', body: 'This is post five' }
+createPost(newPost)
 
 /**Read text**/
 

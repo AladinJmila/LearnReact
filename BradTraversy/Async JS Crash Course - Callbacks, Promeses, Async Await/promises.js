ba74a06732373125d9c42a336b5920bbeaca8f8a
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-07 16:49:42 +0000

    Converting a function that uses 'callbacks' to onw that uses 'promises'

--- BradTraversy/Async JS Crash Course - Callbacks, Promeses, Async Await/promises.js
+++ BradTraversy/Async JS Crash Course - Callbacks, Promeses, Async Await/promises.js
@@ -1,4 +1,31 @@
 
+const posts = [
+  { title: 'Post 1', body: 'This is post one' },
+  { title: 'Post 2', body: 'This is post two' },
+  { title: 'Post 3', body: 'This is post three' },
+  { title: 'Post 4', body: 'This is post four' },
+]
+
+/**Read text**/
+
+function getPosts() {
+  setTimeout(() => {
+    let output = ''
+      posts.forEach((post, index) => {
+        output += `<li>${post.title}</li>`
+      })
+      document.body.innerHTML = output
+  },1000)
+}
+
+/**Read text**/
+
+function createPost(post) {
+  return new Promise((resolve, reject) => {})
+}
+
+/**Read text**/
+
 
 
 /**Read text**/

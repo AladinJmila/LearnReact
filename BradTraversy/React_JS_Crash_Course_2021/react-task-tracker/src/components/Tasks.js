cbd47b0f6becfde129df00b27cee4bfa74b0d384
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-10 08:57:36 +0000

    Added state of the tasks in App.js

--- BradTraversy/React_JS_Crash_Course_2021/react-task-tracker/src/components/Tasks.js
+++ BradTraversy/React_JS_Crash_Course_2021/react-task-tracker/src/components/Tasks.js
@@ -1,26 +1,12 @@
-
-const tasks = [
-  {
-    id: 1,
-    text: 'Doctors Appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Meeting at School',
-    day: 'Feb 6th at 1:30pm',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'Food Shopping',
-    day: 'Feb 5th at 2:30pm',
+const newTask =   {
+    id: 4,
+    text: 'Snooping',
+    day: 'Feb 10th at 00:30am',
     reminder: false,
-  },
-]
+  }
 
-const Tasks = () => {
+
+const Tasks = ({ tasks }) => {
   return (
     <>
      {tasks.map(task => <h3 key={task.id}>{task.text}</h3>)}

5a093886e493c7fb3a6c14d7fa5e691599a521be
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-16 12:19:40 +0000

    M.R. -> ES6 refresher -> Arrow Functions

--- MoshHamedani/MasteringReact/intro-to-react/src/index.js
+++ MoshHamedani/MasteringReact/intro-to-react/src/index.js
@@ -1,11 +1,10 @@
-const person = {
-  name: 'Mosh',
-  walk() {
-    console.log(this)
-  }
-}
+const jobs = [
+  { id: 1, isActive: true },
+  { id: 2, isActive: true },
+  { id: 3, isActive: false },
+]
 
-person.walk()
+const activeJobs = jobs.filter(function(job) { return job.isActive })
+const activeJobsArrow = jobs.filter(job => job.isActive)
 
-const walk = person.walk.bind(person)
-walk()
+console.log(activeJobsArrow)

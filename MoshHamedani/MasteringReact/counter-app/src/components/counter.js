6d478fd9302f7956b57bc9e3bc96205c0302d097
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-17 18:10:10 +0000

    M.R. -> Components -> Binding Event Handlers

--- MoshHamedani/MasteringReact/counter-app/src/components/counter.js
+++ MoshHamedani/MasteringReact/counter-app/src/components/counter.js
@@ -5,12 +5,16 @@ class Counter extends Component {
     count: 0,
   }
 
-  handleIncrement() {
-    console.log('Increment Clicked')
+  // constructor() {
+  //   super()
+  //   this.handleIncrement = this.handleIncrement.bind(this)
+  // }
+
+  handleIncrement = () => {
+    console.log('Increment Clicked', this)
   }
 
   render() {
-
     return (
         <div>
           <span className={this.getBadgeClasses()}>{this.formatCount()}</span>

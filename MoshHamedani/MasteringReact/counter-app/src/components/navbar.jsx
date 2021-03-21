9f77ba200c2bdd9a051d1176b73d54d62fd18cc0
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-21 16:46:13 +0000

    M.R. -> Composing Components -> Mounting Phase

--- MoshHamedani/MasteringReact/counter-app/src/components/navbar.jsx
+++ MoshHamedani/MasteringReact/counter-app/src/components/navbar.jsx
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 
 // Stateless Functional Component:
 const NavBar = ({ totalCounters }) => {
+  console.log('NavBar - Rendered')
+
   return (
-    <nav class="navbar navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
           Navbar{' '}
           <span className="badge badge-pill badge-secondary">
             {totalCounters}

f8a34e0eac723caed87aecc22c4c2b1bb3a47f68
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-24 16:14:48 +0000

    M.R. -> Sorting -> Adding the Sort Icon

--- MoshHamedani/MasteringReact/vidly/src/components/common/tableHeader.jsx
+++ MoshHamedani/MasteringReact/vidly/src/components/common/tableHeader.jsx
@@ -5,7 +5,7 @@ import React, { Component } from 'react'
 // onSort: function
 
 class TableHeader extends Component {
-  raiseSort = (path) => {
+  raiseSort = path => {
     const sortColumn = { ...this.props.sortColumn }
     if (sortColumn.path === path)
       sortColumn.order = sortColumn.order === 'asc' ? 'desc' : 'asc'
@@ -17,16 +17,24 @@ class TableHeader extends Component {
     this.props.onSort(sortColumn)
   }
 
+  renderSortIcon = column => {
+    if (column.path !== this.props.sortColumn.path) return null
+    if (this.props.sortColumn.order === 'asc')
+      return <i className='fa fa-sort-asc'></i>
+    return <i className='fa fa-sort-desc'></i>
+  }
+
   render() {
     return (
       <thead>
         <tr>
-          {this.props.columns.map((column) => (
+          {this.props.columns.map(column => (
             <th
+              className='clickable'
               key={column.path || column.key}
               onClick={() => this.raiseSort(column.path)}
             >
-              {column.label}
+              {column.label} {this.renderSortIcon(column)}
             </th>
           ))}
         </tr>

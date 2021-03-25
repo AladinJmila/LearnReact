7a10e67f68a8f9ebbe4db97172082a1a293fe657
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-25 17:10:51 +0000

    M.R. -> Routing -> Query String Parameters

--- MoshHamedani/MasteringReact/router-app/src/components/posts.jsx
+++ MoshHamedani/MasteringReact/router-app/src/components/posts.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import QueryString from 'query-string'
 
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
+  const result = QueryString.parse(location.search)
+  console.log(result)
+
   return (
     <div>
       <h1>Posts</h1>

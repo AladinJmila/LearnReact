107888dfc949a0d6c833652da778582e1dd97a64
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-25 10:31:32 +0000

    Fixing Repo

--- MoshHamedani/MasteringReact/router-app/src/App.js
+++ MoshHamedani/MasteringReact/router-app/src/App.js
@@ -18,9 +18,7 @@ class App extends Component {
           <Switch>
             <Route
               path='/products'
-              render={props => (
-                <Products sortBy='newest' GitUpdate='testing' {...props} />
-              )}
+              render={props => <Products sortBy='newest' {...props} />}
             />
             <Route path='/posts' component={Posts} />
             <Route path='/admin' component={Dashboard} />

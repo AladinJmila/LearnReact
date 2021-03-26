37161c681532a28f258b58dddeb55830c0acc134
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-26 13:55:18 +0000

    M.R. -> Forms -> Building a Bootstap Form

--- MoshHamedani/MasteringReact/vidly/src/App.js
+++ MoshHamedani/MasteringReact/vidly/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Route, Redirect, Switch } from 'react-router-dom'
 import MovieForm from './components/movieForm'
 import Movies from './components/movies'
@@ -5,7 +6,7 @@ import Customers from './components/customers'
 import Rentals from './components/rentals'
 import NotFound from './components/notFound'
 import NavBar from './components/navBar'
-import React from 'react'
+import LoginForm from './components/loginForm'
 import './App.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <NavBar />
       <main className='container'>
         <Switch>
+          <Route path='/login' component={LoginForm}></Route>
           <Route path='/movies/:id' component={MovieForm}></Route>
           <Route path='/movies' component={Movies}></Route>
           <Route path='/customers' component={Customers}></Route>

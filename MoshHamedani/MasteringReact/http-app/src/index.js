80b35b4977035fb33d8b5f4e1affcf6fd981daa3
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-03-31 07:56:24 +0100

    M.R. -> Calling Backend Services -> Logging Errors

--- MoshHamedani/MasteringReact/http-app/src/index.js
+++ MoshHamedani/MasteringReact/http-app/src/index.js
@@ -1,9 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+Sentry.init({
+  dsn:
+    'https://9139f2706fa84499a953a7afea87d4cf@o561814.ingest.sentry.io/5699638',
+  autoSessionTracking: false,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+})
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()

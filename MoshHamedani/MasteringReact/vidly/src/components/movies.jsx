e90b47f787ce0066ee2520aea0fd804e67677a35
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-04-02 09:55:31 +0100

    M.R. -> Authentication and Authorization -> Showing or Hiding Elements based on the User

--- MoshHamedani/MasteringReact/vidly/src/components/movies.jsx
+++ MoshHamedani/MasteringReact/vidly/src/components/movies.jsx
@@ -102,6 +102,7 @@ class Movies extends Component {
       sortColumn,
       searchQuery,
     } = this.state
+    const { user } = this.props
 
     if (count === 0) return <p>There are no movies in the database.</p>
 
@@ -117,16 +118,19 @@ class Movies extends Component {
           />
         </div>
         <div className='col'>
-          <Link
-            to='/movies/new'
-            className='btn btn-primary'
-            style={{ marginButtom: 20 }}
-          >
-            New Movie
-          </Link>
+          {user && (
+            <Link
+              to='/movies/new'
+              className='btn btn-primary'
+              style={{ marginButtom: 20 }}
+            >
+              New Movie
+            </Link>
+          )}
           <p>Showing {totalCount} movies in the database</p>
           <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
+            user={user}
             movies={movies}
             sortColumn={sortColumn}
             onLike={this.handleLike}
--- MoshHamedani/MasteringReact/vidly/src/components/moviesTable.jsx
+++ MoshHamedani/MasteringReact/vidly/src/components/moviesTable.jsx
@@ -21,14 +21,15 @@ class MoviesTable extends Component {
     },
     {
       key: 'delete',
-      content: movie => (
-        <button
-          onClick={() => this.props.onDelete(movie)}
-          className='btn btn-danger btn-sm'
-        >
-          Delete
-        </button>
-      ),
+      content: movie =>
+        this.props.user && (
+          <button
+            onClick={() => this.props.onDelete(movie)}
+            className='btn btn-danger btn-sm'
+          >
+            Delete
+          </button>
+        ),
     },
   ]
 

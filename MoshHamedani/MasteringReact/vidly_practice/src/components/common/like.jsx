f3a4b0a087369984d8475bc32434b1220c03d70b
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-04-05 19:41:45 +0100

    Vidly Practice -> Like Component

---
+++ MoshHamedani/MasteringReact/vidly_practice/src/components/common/like.jsx
@@ -0,0 +1,14 @@
+const Like = () => {
+  let classes = 'fa fa-heart'
+  if (!this.props.liked) classes += '-o'
+  return (
+    <i
+      onClick={this.props.onClick}
+      className={classes}
+      style={{ cursor: 'pointer' }}
+      aria-hidden='true'
+    ></i>
+  )
+}
+
+export default Like
